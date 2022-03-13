7e139797c83dedd0f2da99874d781075906c3f19
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-13 15:52:22 +0300

    add model

--- app/models.py
+++ app/models.py
@@ -1,11 +1,4 @@
-from . import db
-
-#...
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-
-    def __repr__(self):
-        return f'User {self.username}'
+from . import db 
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin, current_user
+from . import login_manager
