7b5e60c04687dbf9d4bbf96dd277c254c9740425
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-13 14:39:11 +0300

    add db sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
 bootstrap = Bootstrap()
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+    #...
+def create_app(config_name):
+    app = Flask(__name__)
