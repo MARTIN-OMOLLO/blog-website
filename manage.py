70f868cb4f113ec6ac57b5c08a15b1afd627eed2
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-13 14:17:45 +0300

    add manager and server

--- manage.py
+++ manage.py
@@ -3,3 +3,9 @@ from app.models import User
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 app = create_app('production')
+
+manager = Manager(app)
+
+manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
