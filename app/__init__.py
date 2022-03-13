1bb32ccdbd987616a9400d023ee3f4cc5fd0c8df
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-13 14:42:05 +0300

    add import main

--- app/__init__.py
+++ app/__init__.py
@@ -12,3 +12,25 @@ login_manager.login_view = 'auth.login'
     #...
 def create_app(config_name):
     app = Flask(__name__)
+    #....
+
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://martin:MARtin1999@localhost/blog'
+    
+
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app 
+#....
