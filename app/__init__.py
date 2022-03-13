63eaa725ebedd0fdecdded947c3ff6cff6c67fd4
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-13 14:37:22 +0300

    add flask import in init.py

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from config import config_options
+bootstrap = Bootstrap()
--- config.py
+++ config.py
@@ -8,4 +8,29 @@ class Config:
     @staticmethod
     def init_app(app):
         pass
+
+    class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATA_URL')
+    
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martin:martin123@localhost/blog'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
     
